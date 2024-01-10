dcbb56b450f222f66994349c808d890c69675054
Alan Roybal <alanroybal05@gmail.com>
2024-01-10 14:37:32 -0600

    refactoring and added tiles files

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -27,11 +27,6 @@ public class GamePanel extends JPanel implements Runnable {
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
-    
-    //player default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
 
     public GamePanel() {
 
