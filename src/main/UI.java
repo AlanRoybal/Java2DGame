023b9e83612ca6de1fddc7e9e184c7a4ddfd7a1b
Alan Roybal <alanroybal05@gmail.com>
2024-01-18 10:38:09 -0600

    changed ui to show yarn ball image

--- src/main/UI.java
+++ src/main/UI.java
@@ -3,22 +3,29 @@ package main;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import item.ITEM_Yarn;
 
 public class UI {
     
     GamePanel gp;
     Font arial_40;
+    BufferedImage keyImage;
 
     public UI(GamePanel gp) {
         this.gp = gp;
 
         arial_40 = new Font("Arial", Font.PLAIN, 40);
+        ITEM_Yarn yarn = new ITEM_Yarn();
+        keyImage = yarn.image;
     }
 
     public void draw(Graphics2D g2) {
 
         g2.setFont(arial_40);
         g2.setColor(Color.white);
-        g2.drawString("Key = "+ gp.player.hasKey, 50, 50);
+        g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
+        g2.drawString("x "+ gp.player.hasKey, 85, 60);
     }
 }
